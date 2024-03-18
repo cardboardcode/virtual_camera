6354c49d1ad6f6468d8cf4a10c064ec148de74da
Gary Bey <beyhy94@gmail.com>
2024-03-18 11:25:32 +0800

    Revived Code Coverage Functionality (#10)
    * :hammer: Testing experimental code-coverage inclusive GitHub Action CI.
    
    Signed-off-by: Bey Hao Yun <beyhy94@gmail.com>
    
    * :heavy_plus_sign: Added seperate coveralls_ci_action.yml to isolate code coverage workflow for better debugging.
    
    Signed-off-by: Bey Hao Yun <beyhy94@gmail.com>
    
    * :hammer: Confirmed baseline manual ROS 2 CI. Now testing code coverage feature with Coveralls.io.
    
    Signed-off-by: Bey Hao Yun <beyhy94@gmail.com>
    
    * :hammer: Changed from Coveralls to Codecov. Fixing failing CI.
    
    Signed-off-by: Bey Hao Yun <beyhy94@gmail.com>
    
    * :hammer: CI now passing. Adding back industrial_ci integration.
    
    Signed-off-by: Bey Hao Yun <beyhy94@gmail.com>
    
    * :hammer: Added comments in ci.sh for better understanding. Removed debug colcon version-check statement.
    
    Signed-off-by: Bey Hao Yun <beyhy94@gmail.com>
    
    * :blue_book: Added back working Codecov badge on root README.md.
    
    Signed-off-by: Bey Hao Yun <beyhy94@gmail.com>
    
    ---------
    
    Signed-off-by: Bey Hao Yun <beyhy94@gmail.com>

---
+++ ci.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Install dependencies
+sudo apt-get update
+sudo apt-get install -y lcov curl
+sudo apt-get install -y python3-pip
+pip install -U colcon-common-extensions
+pip install -U colcon-lcov-result
+
+# Install ROS 2 dependencies
+rosdep update
+rosdep install -y --from-paths ./ --ignore-src --rosdistro humble
+
+# Build virtual_camera ROS 2 package
+source /opt/ros/humble/setup.bash
+colcon build --symlink-install --cmake-args \
+            -DCMAKE_CXX_FLAGS='-fprofile-arcs -ftest-coverage' \
+            -DCMAKE_C_FLAGS='-fprofile-arcs -ftest-coverage'
+
+# Generate code coverage report of virtual_camera ROS 2 package gtests
+colcon lcov-result --initial
+colcon test --packages-select virtual_camera
+colcon lcov-result
