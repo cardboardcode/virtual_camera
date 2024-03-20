cca8ea0fd27029bb2b92cfa76a10d0f7d38df7a0
Bey Hao Yun <beyhy94@gmail.com>
2024-03-20 12:13:14 +0800

    :heavy_plus_sign: Added code coverage for foxy_devel. Verified locally.
    Signed-off-by: Bey Hao Yun <beyhy94@gmail.com>

---
+++ ci.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Install dependencies
+sudo apt-get update
+sudo apt-get install -y lcov curl
+sudo apt-get install -y python3-pip
+pip install -U colcon-common-extensions
+pip install -U colcon-lcov-result
+
+# Install ROS 2 dependencies
+rosdep update
+rosdep install -y --from-paths ./ --ignore-src --rosdistro foxy
+sudo apt-get install -y libopencv-dev
+sudo apt-get install -y ros-foxy-cv-bridge
+
+# Build virtual_camera ROS 2 package
+source /opt/ros/foxy/setup.bash
+colcon build --symlink-install --cmake-args \
+            -DCMAKE_CXX_FLAGS='-fprofile-arcs -ftest-coverage' \
+            -DCMAKE_C_FLAGS='-fprofile-arcs -ftest-coverage'
+
+# Generate code coverage report of virtual_camera ROS 2 package gtests
+colcon lcov-result --initial
+colcon test --packages-select virtual_camera
+colcon lcov-result
