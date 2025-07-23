174519cc02b9b507872e754ea17e5f58f509088b
gary.bey <gary.bey@kabam.ai>
2025-07-23 21:00:46 +0800

    :hammer: Optimised loading of data images/videos. Previously it was raw-copy9ing.
    Signed-off-by: gary.bey <gary.bey@kabam.ai>

--- scripts/2_create_docker_container.bash
+++ scripts/2_create_docker_container.bash
@@ -8,6 +8,7 @@ docker run -it --rm \
     -e DISPLAY=$DISPLAY \
     -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v ./data:/virtual_camera_ws/src/virtual_camera/data \
 vcam_image:humble bash -c \
 "ros2 launch virtual_camera showimageraw.launch.py \
 use_image_viewer:=true"
